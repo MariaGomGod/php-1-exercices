1cef5d321cfc395f4a289681d411fb119e4360b9
Maria Gomez <maria_gg86@yahoo.es>
2021-06-07 12:47:24 +0200

    aplicados estilos a index.php

--- public/index.php
+++ public/index.php
@@ -4,18 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/main.css" type="text/css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Hola, Carlos</h1>
-    <p>Hoy es 
-    <strong>
-        <?php
-         
-         $today = getdate();
-         echo $today['weekday'];
 
-        ?>
-    </strong>. ¿Qué tal estás?</p>
+    <?php
+    require("./links.php");
+    ?>
+    
+    <?php
+    $weeekDays = [
+        1 => 'lunes',
+        2 => 'martes',
+        3 => 'miércoles',
+        4 => 'jueves',
+        5 => 'viernes',
+        6 => 'sábado',
+        7 => 'domingo',
+    ];
+    ?>
+
+    <?php
+        var_dump($_GET);
+    ?>
+    <h1>Hola, <?php echo $_GET['name']; ?></h1>
+    <p>Hoy es <strong><?= $weeekDays[date('N')]; ?></strong>. ¿Qué tal estás?</p>
+
+    <script src="/assets/main.js"></script>
 </body>
 </html>
---
+++ public/links.php
@@ -0,0 +1,2 @@
+<a href="http://localhost:8080/">Index</a>
+<a href="http://localhost:8080/employees.php">Employees</a>
