4559f6bbe7ba323b27cd4ccb9b61b43dee76f786
Maria Gomez <maria_gg86@yahoo.es>
2021-06-09 13:40:37 +0200

    añadido header e insercion

---
+++ public/employees_add.php
@@ -0,0 +1,18 @@
+<?php
+
+require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
+
+$query = 'INSERT INTO `employees` (`name`, `email`, `age`, `city`) VALUES (:nombre, :correo, :edad, :ciudad);';
+
+$stm = $dbConnexion->prepare($query); // prepare() prepara la consulta pero no la ejecuta
+
+$stm->bindParam(':nombre', $_POST['name']);
+$stm->bindParam(':correo', $_POST['email']);
+$stm->bindParam(':edad', $_POST['age']);
+$stm->bindParam(':ciudad', $_POST['city']);
+
+$stm->execute(); // excute() ejecuta la consulta
+
+header('Location: /employees.php?message' . urlencode(('El usuario ' .$_POST['email'].' se ha añadido correctamente.')));
+
+exit();
--- public/employees_html.php
+++ public/employees_html.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejercicio PHP</title>
 </head>
 
 <body>
@@ -28,15 +28,35 @@
         <tbody>
             <?php foreach ($people as $person) : ?>
                 <tr>
-                    <td><?php echo $person['id'] ?></td>
+                    <td><a href="/employees.php?id=<?= $person['id']?>"><?= $person['id']?></a></td>
                     <td><?php echo $person['name'] ?></td>
-                    <td><a href="/employees.php?id=<?= $person['id']?>"><?= $person['email']?></a></td>
+                    <td><a href="/employees.php?id=<?= $person['email']?>"><?= $person['email']?></a></td>
                     <td><?php echo $person['city'] ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
     </table>
 
+    <?php if (isset($_GET['message'])) :?>
+        <p><?= $_GET['message']; ?></p>
+    <?php endif; ?>
+            
+    <hr/>
+    <form method="POST" action="/employees_add.php" enctype="multipart/form-data">
+        <label for="name">Nombre</label>
+        <input type="text" id="name" name="name" required/>
+        <br/>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required/>
+        <br/>
+        <label for="age">Edad</label>
+        <input type="number" id="age" name="age" required/>
+        <br/>
+        <label for="archivo">Archivo</label>
+        <input type="file" id="archivo" name="archivo" />
+        <hr/>
+        <input type="submit" value="Enviar"/>
+    </form>
 </body>
 
 </html>
