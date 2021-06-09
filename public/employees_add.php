c8c684cc25b08737c8f175c7f31841df6a2e40b5
Maria Gomez <maria_gg86@yahoo.es>
2021-06-09 13:45:33 +0200

    añadida alternativa de pasar parametros

--- public/employees_add.php
+++ public/employees_add.php
@@ -3,13 +3,19 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
 
 $query = 'INSERT INTO `employees` (`name`, `email`, `age`, `city`) VALUES (:nombre, :correo, :edad, :ciudad);';
+$params = [
+    ':nombre' => $_POST['name'],
+    ':correo' => $_POST['email'],
+    ':edad' => $_POST['age'],
+    ':ciudad' => $_POST['city'],
+];
 
 $stm = $dbConnexion->prepare($query); // prepare() prepara la consulta pero no la ejecuta
 
-$stm->bindParam(':nombre', $_POST['name']);
+/* $stm->bindParam(':nombre', $_POST['name']);
 $stm->bindParam(':correo', $_POST['email']);
 $stm->bindParam(':edad', $_POST['age']);
-$stm->bindParam(':ciudad', $_POST['city']);
+$stm->bindParam(':ciudad', $_POST['city']); */
 
 $stm->execute(); // excute() ejecuta la consulta
 
