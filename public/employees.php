be49bed81da16a914004b222b814a5025758bfe6
Maria Gomez <maria_gg86@yahoo.es>
2021-06-09 12:43:20 +0200

    cambiar el metodo de consulta, utilizar prepare en lugar de query

--- public/employees.php
+++ public/employees.php
@@ -1,12 +1,24 @@
 <?php
-    require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
 
-    $query = (isset($_GET['id'])) ? 'SELECT * FROM employees WHERE id=' .$_GET['id'] : 'SELECT * FROM employees'; // isset() nos da información de si una variable está definida y si un valor está definido
+ $query = 'SELECT * FROM employees';
+if (isset($_GET['id'])) {
+    $query = 'SELECT * FROM employees WHERE id = :identificador';
+} elseif (isset($_GET['email'])) {
+    $query = 'SELECT * FROM employees WHERE email = :correo';
+}
+$stm = $dbConnexion->prepare($query); // prepare() es para evitar la inyección de sql
 
-    $stm = $dbConnexion->query($query);
-    $people = $stm->fetchAll(PDO::FETCH_ASSOC);
+ if (isset($_GET['id'])) {
+     $stm->bindParam(':identificador', $_GET['id']); // con bindParam() sustituimos "identificador" por el parámetro que yo le pase, en este caso "id"
+ } elseif (isset($_GET['email'])) {
+     $stm->bindParam(':correo', $_GET['email']);
+ }
+
+ $stm->execute();
+
+ $people = $stm->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <?php
 
 require $_SERVER['DOCUMENT_ROOT'] . '/employees_'.(isset($_GET['format']) && in_array($_GET['format'], ['json', 'xml']) ? $_GET['format'] : 'html').'.php';
-
