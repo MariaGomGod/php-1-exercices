251a622b1911ab963c73c7dafd25a31033575087
Maria Gomez <maria_gg86@yahoo.es>
2021-06-10 11:25:31 +0200

    añadir logica en el fichero employees_delete.php para eliminar un registro

--- public/employees_delete.php
+++ public/employees_delete.php
@@ -1,9 +1,24 @@
 <?php
 
+require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
+
 $body = file_get_contents('php://input'); // leemos el cuerpo (body) de la solicitud http
 
-$person = json_decode($body, true); // transformamos la cadena a un objeto json a través de json_decode()
+$person = json_decode($body, true); // json_decode () convierte un objeto json en un objeto PHP
 
-echo json_encode($person);
+$query = 'DELETE FROM employees WHERE id = :identificador';
+
+$stm = $dbConnexion->prepare($query);
+$stm->bindParam(':identificador', $person['id']);
+$stm->execute();
+
+$result = $stm->rowCount();
+
+$response = [
+    'status' => $stm->rowCount() === 0 ? 'error' : 'success',
+    'message' => $stm->rowCount() === 0 ? 'No hemos eliminado a nadie' : 'He eliminado '.$stm->rowCount(). ' fila',
+];
+
+echo json_encode($response); // json_encode() permite convertir datos PHP en formato json
 
 exit();
