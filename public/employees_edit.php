a1627dc4c1160242f682f181731bbb55445c1be3
Maria Gomez <maria_gg86@yahoo.es>
2021-06-11 09:47:44 +0200

    solucion a la edicion de empleados de employees_edit.php

--- public/employees_edit.php
+++ public/employees_edit.php
@@ -1,24 +1,24 @@
 <?php
 
-require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
+// print_r($_POST);
 
-$query = 'INSERT INTO `employees` (`name`, `email`, `age`, `city`) VALUES (:nombre, :correo, :edad, :ciudad);';
+require $_SERVER["DOCUMENT_ROOT"] . "/lib/app.php";
+
+$query = "UPDATE employees SET name=:nombre, email=:correo, age=:edad, city=:ciudad WHERE id=:id"; 
+
 $params = [
-    ':nombre' => $_POST['name'],
-    ':correo' => $_POST['email'],
-    ':edad' => $_POST['age'],
-    ':ciudad' => $_POST['city'],
+    ":id" => $_POST["id"],
+    ":nombre" => $_POST["name"],
+    ":correo" => $_POST["email"],
+    ":edad" => $_POST["age"],
+    ":ciudad" => $_POST["city"]
 ];
 
-$stm = $dbConnexion->prepare($query); 
 
-$stm->execute($params); 
+$stm = $dbConnection->prepare($query);
 
-print_r($_POST);
+$stm->execute($params);
 
-header('Location: /employees.php?message' . urlencode(('El usuario ' .$_POST['email'].' se ha actualizado correctamente.')));
+header('Location: /employees.php?message=' . urlencode('El usuario '.$_POST['name'].' se ha modificado.'));
 
-exit();
-
-// Ejercicio: actualizará los datos del usuario. Hacer query parecida a la de employees_add.php
 
